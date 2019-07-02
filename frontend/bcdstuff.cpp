114c1d0ffa442f4fb97faa2ecf55319b6338d408
Michael A. Uman <michael.uman@wunderbar.com>
2019-07-02 12:49:30 -0700

    Moved resources into new directory

--- frontend/bcdstuff.cpp
+++ frontend/bcdstuff.cpp
@@ -1,13 +1,20 @@
 #include <QtCore>
+#include "bcdstuff.h"
 
-
-
+/**
+ * @brief Convert a BCD byte to a decimal byte.
+ * @param bcd
+ * @return
+ */
 uint8_t bcd_to_decimal(uint8_t bcd) {
-    uint8_t value = ((((bcd & 0xf0) >> 4) * 10) + ((bcd & 0x0f)));
-
-    return value;
+    return ((((bcd & 0xf0) >> 4) * 10) + ((bcd & 0x0f)));;
 }
 
+/**
+ * @brief Convert a decimal byte to a BCD byte.
+ * @param dec
+ * @return
+ */
 uint8_t decimal_to_bcd(uint8_t dec) {
     uint8_t msn = static_cast<uint8_t>(dec/10);
     uint8_t lsn = static_cast<uint8_t>(dec%10);
